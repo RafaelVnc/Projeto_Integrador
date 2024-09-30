7e5472c6dae2cabdd95c7e0e5447ba8d423ca14a
Rafael Venancio <rafael.venancio08.06@gmail.com>
2024-09-30 11:56:41 -0300

    feat: Adicionado CRUD dos itens do cardápio & fix: url css meu_cardapio

--- PecaAi/controllers/cardapio.controller.js
+++ PecaAi/controllers/cardapio.controller.js
@@ -1,5 +1,6 @@
 const models = require('../models');
 
+//Post novo item do cardápio
 function addItem(req, res){
     const item = {
         nome:req.body.nome,
@@ -23,6 +24,73 @@ function addItem(req, res){
     });
 }
 
+//Get apenas um item
+function item(req, res){
+    const id = req.params.id;
+
+    models.itemCardapio.findByPk(id).then(result => {
+        res.status(200).json(result);
+    }).catch(error => {
+        res.status(500).json({
+            message: "Algo deu errado!"
+        })
+    });
+}
+
+//Get de todos os itens do cardápio
+function itens(req, res){
+    models.itemCardapio.findAll().then(result => {
+        res.status(200).json(result);
+    }).catch(error => {
+        res.status(500).json({
+            message: "Algo deu errado!"
+        })
+    });
+}
+
+//Update de um item do cardápio
+function editItem(req, res){
+    const id = req.params.id;
+    const updatedItem = {
+        nome:req.body.nome,
+        descricao:req.body.descricao,
+        preco:req.body.preco,
+        fotoUrl:req.body.foto
+    }
+
+    models.itemCardapio.update(updatedItem, {where: {id:id}}).then(result =>{
+        res.status(200).json({
+            message: "Item atualizado!",
+            item: updatedItem
+        });
+    }).catch(error => {
+        res.status(500).json({
+            message: "Algo deu errado!",
+            error: error
+        });
+    });
+}
+
+//Delete de um item do cardápio
+function deleteItem(req, res){
+    const id = req.params.id;
+
+    models.itemCardapio.destroy({where:{id:id}}).then(result => {
+        res.status(200).json({
+            message: "Item deletado!"
+        });
+    }).catch(error => {
+        res.status(500).json({
+            message: "Algo deu errado!",
+            error: error
+        });
+    });
+}
+
 module.exports= {
-    addItem: addItem
+    addItem: addItem, 
+    item: item,
+    itens: itens, 
+    editItem: editItem,
+    deleteItem: deleteItem
 }
--- PecaAi/routes/cardapio.js
+++ PecaAi/routes/cardapio.js
@@ -4,5 +4,9 @@ const cardapioController = require('../controllers/cardapio.controller');
 const router = express.Router();
 
 router.post("/cadastrarProduto", cardapioController.addItem);
+router.get("/", cardapioController.itens);
+router.get("/:id", cardapioController.item);
+router.patch("/:id", cardapioController.editItem);
+router.delete("/:id", cardapioController.deleteItem);
 
 module.exports = router;
